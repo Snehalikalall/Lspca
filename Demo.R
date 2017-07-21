0fdfde5bfdfe81efc3bfb2ebe216b5d4ed51a2b1
Snehalikalall <snehaisha123@gmail.com>
2017-07-21 14:53:55 +0530

    Add files via upload

---
+++ Demo.R
@@ -0,0 +1,27 @@
+# Run the demo code, We have given a normalized PBMC data as example Or put the normalized final data here
+
+
+# Load the Raw data to prerprocess in "data" folder. Genes should be in row and Samples should be in coloumn. Gene names must be there.
+Data<- read.csv("data/Rawdata.csv", header=FALSE)
+
+#Calling the Preprocess R code if tehre is Raw dataset
+source("src/PreProcess.R")
+
+
+# Execute the functions
+source("src/Functions.R")
+
+# Call the python Code.'5' is number of iterations(k) in python code for example Dataset, Depends upon size on datasets.
+PythonCall()
+
+# Read the python ouput
+Data_Sample <- read.csv("data/datares.csv", header=FALSE)
+
+# Do the PCA
+Transformed_Mat<-PcaCall(Data_Sample)
+
+# Do the Projection
+Data_Projected<-ProjectionCall(Transformedmat)
+
+# Do the Plot, It is only for Example Dataset, where annotation is Given
+PlotCall(Datamat)
